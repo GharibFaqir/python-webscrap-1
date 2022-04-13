dc1b63d278e5c9a3247468b1d5b25626d290a927
Sekiei <taimur.shabbir@gmail.com>
2022-04-13 20:24:27 +0500

    White House Briefings and Statements Printed Attempted on own, now checking with tutorial 1. Briefing printed separate 2. Statements printed separate

--- whitehouse-bs.py
+++ whitehouse-bs.py
@@ -1,1 +1,32 @@
-# Scrapping Website: https://www.whitehouse.gov/briefing-room/statements-releases/
+# Scrapping White House's Briefings and Statements Website
+# Ref: https://www.whitehouse.gov/briefing-room/statements-releases/
+
+# Goal: Extract all links that point to the briefings and statements
+import requests
+from bs4 import BeautifulSoup
+
+webpage = requests.get(
+    "https://www.whitehouse.gov/briefing-room/statements-releases/")
+print("1. Website Response: ")
+print(webpage.status_code)
+print()
+
+src = webpage.content
+soup = BeautifulSoup(src)
+links = soup.find_all("a")
+
+print("2. Briefings: ")
+for bs in links:
+    if "Briefing" in bs.text:
+        print(bs)
+        print(bs.attrs['href'])
+
+print()
+
+print("3. Statements: ")
+for bs in links:
+    if "Statement" in bs.text:
+        print(bs)
+        print(bs.attrs['href'])
+
+print()
