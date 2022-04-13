413b0d593a4fe90ebd5648c807d1c8b9fe0ee5a5
Sekiei <taimur.shabbir@gmail.com>
2022-04-13 15:00:18 +0500

    Get Webpage Content, All Links and specific Links 1. Get all the content available on the web page 2. Get all the links available on the Webpage 3. Get specific links available on the Webpage

--- soup-request.py
+++ soup-request.py
@@ -12,11 +12,40 @@ result = requests.get("https://www.google.com/")
 
 # If website is accessible
 # than response = 200 OK
+print("1. Webpage Response: ")
 print(result.status_code)
-
+print()
 # Other HTTPS Status Codes:
 # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 
 # Access header of webpage, to make sure
 # we have accessed the correct website
+print("2. HTTP Headers: ")
 print(result.headers)
+print()
+
+# Accessing content on webpage
+src = result.content
+print("3. Webpage Content: ")
+print(src)
+print()
+
+# Beautify raw data through soup
+soup = BeautifulSoup(src)
+
+# To get all the links from webpage
+print("4. Print all links: ")
+links = soup.find_all("a")
+print(links)
+print()
+
+# Get specific link
+print("5. Get link and href: ")
+for x in links:
+    if "About" in x.text:
+        print(x)
+        print(x.attrs['href'])
+
+print()
+
+#
